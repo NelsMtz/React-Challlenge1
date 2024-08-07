de5b65b800fc698d2dcc854fdf0dcb447838b3bc
Nelly <nmatu308@gmail.com>
2024-08-07 18:41:07 +0300

    created sort component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,13 @@ import  {useEffect, useState} from 'react'
 import TransactionList from './TransactionList';
 import Form from './Form';
 import SearchTransaction from './SearchTransaction';
+import Sort from './Sort';
 
 function Home() {
     
     const [transactions, setTransactions] = useState ([]);
     const [searchQuery, setSearchQuery] = useState ([]);
+    const [sortOption, setSortOption] = useState ([]);
 
     useEffect (()  => {
         fetch( "http://localhost:3000/transactions")
@@ -28,12 +30,29 @@ function Home() {
         .then(data => setTransactions([...transactions, data]))
         .catch(error => console.log(error))
     };
-        
+
+    const handleSortChange = (e)=> {
+      setSortOption (e.target.value)
+    }
+     
+    const sortedTransactions = [...transactions].sort((a, b) => {
+      if (sortOption === 'category') {
+        return a.category.localeCompare(b.category)
+      } else if (sortOption === 'description') {
+        return a.description.localeCompare(b.description)
+      } else {
+        return new Date(a.date) - new Date(b.date)
+      }
+    });
+  
+      
+
   return (
     <div className='home'> 
         <header>Bank of Flatiron</header>  
         <Form  addTransaction ={addTransaction}/> 
         <SearchTransaction  searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>      
+        <Sort sortOption={sortOption} onChange ={handleSortChange}  />
         <TransactionList   transactions={transactions}/>  
         
     </div>
---
+++ src/components/Sort.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sort({sortOption, onSortChange}) {
+  return (
+    <select className="" onChange={onSortChange} value={sortOption}>
+        <option value="date">Date</option>
+        <option value="description">Description</option>
+        <option value="category">Category</option>
+    </select>
+
+
+
+  )
+}
+
+export default Sort
